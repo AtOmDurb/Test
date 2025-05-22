c31df7a0bd8579d9c1c46795d883e77c159f1317
AtOmDurb <egor30rus@inbox.ru>
2025-05-22 17:20:56 +0400

    прав

---
+++ client/src/components/Dashboard/TeacherPanel.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+
+const StudentPanel = () => {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <div className="dashboard">
+      <h1>Панель Учиеля</h1>
+      <p>Добро пожаловать, {user?.username}!</p>
+      <button onClick={logout}>Выйти</button>
+    </div>
+  );
+};
+
+export default StudentPanel;
